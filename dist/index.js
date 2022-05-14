a2f50db079d033bf541146fe8d112575752481aa
XanderBits <amarettosyntax@protonmail.com>
2022-05-14 04:02:59 -0400

    [UPDATE]

--- dist/index.js
+++ dist/index.js
@@ -12,15 +12,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-//import "reflect-metadata";
+require("reflect-metadata");
 require("dotenv/config");
 const app_1 = __importDefault(require("./app"));
-//import { AppDataSource } from "./db/data-source";
+const data_source_1 = require("./db/data-source");
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        /*await AppDataSource.initialize()
-        .then(() => console.log("**SUCCESSFULLY CONNECTED TO DATABASE**"))
-        .catch((err) => console.error(err));*/
+        yield data_source_1.AppDataSource.initialize()
+            .then(() => console.log("**SUCCESSFULLY CONNECTED TO DATABASE**"))
+            .catch((err) => console.error(err));
         const PORT = process.env.PORT || 3000;
         app_1.default.listen(PORT, () => console.log(`Server listening on PORT:${PORT}`));
     });
