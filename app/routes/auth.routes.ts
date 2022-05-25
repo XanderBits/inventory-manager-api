d0e2d1d67a6bbfe5603be006614c41f5318b0287
XanderBits <amarettosyntax@protonmail.com>
2022-05-25 19:42:42 -0400

    [UPDATE] Auth routes

--- app/routes/auth.routes.ts
+++ app/routes/auth.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
-import  register from '../controllers/auth/register'
+import  {register} from '../controllers/auth/register'
+import { validateRegister } from '../controllers/auth/validators/validateRegister'
 const router = Router()
 
-router.post('/auth/register', register)
+router.post('/auth/register', validateRegister, register)
 
 export default router
