3c30e6a67f8a5811c183c73fdc217e0d5c4e87a5
XanderBits <amarettosyntax@protonmail.com>
2022-05-26 03:04:30 -0400

    [UPDATE] Migration Querys

--- app/migrations/1653460148102-CreateinitRole.ts
+++ app/migrations/1653460148102-CreateinitRole.ts
@@ -3,12 +3,12 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class CreateinitRole1653460148102 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`INSERT INTO "Roles"(id,name) VALUES (1,'Administrador');`),
-        await queryRunner.query(`INSERT INTO "Roles"(id,name) VALUES (2,'User');`)
+        await queryRunner.query(`INSERT INTO Roles(id,name) VALUES (1,'Administrador');`),
+        await queryRunner.query(`INSERT INTO Roles(id,name) VALUES (2,'User');`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DELETE FROM "Roles" WHERE id = 1;`),
-        await queryRunner.query(`DELETE FROM "Roles" WHERE id = 2;`)
+        await queryRunner.query(`DELETE FROM Roles WHERE id = 1;`),
+        await queryRunner.query(`DELETE FROM Roles WHERE id = 2;`)
     }
 }
--- app/migrations/1653460170008-CreateUserRoot.ts
+++ app/migrations/1653460170008-CreateUserRoot.ts
@@ -3,11 +3,11 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class CreateUserRoot1653460170008 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`INSERT INTO "Users"(id,name,password,role) 
-                                VALUES (1,'UserRoot','useRoot!26528',1);`
+        await queryRunner.query(`INSERT INTO users(id,name,lastname,email,password,"roleId") 
+        VALUES(1,'User','Root','${process.env.USER_ROOT_EMAIL}','${process.env.USER_ROOT_PASSWORD}',1);`
         )
     }
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DELETE FROM "Users" WHERE id = 1;`)
+        await queryRunner.query(`DELETE FROM Users WHERE id = 1;`)
     }
 }
