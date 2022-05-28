ef7ec1197cdbfb72a043763317b7878c1fbd9769
XanderBits <amarettosyntax@protonmail.com>
2022-05-28 19:08:20 -0400

    [ADD] Auth Login Route

--- app/routes/auth.routes.ts
+++ app/routes/auth.routes.ts
@@ -1,8 +1,11 @@
-import { Router } from "express"
+import { Request, Response, Router } from "express"
 import  {register} from '../controllers/auth/register'
 import { validateRegister } from '../controllers/auth/validators/validateRegister'
 const router = Router()
 
 router.post('/auth/register', validateRegister, register)
+router.post('/auth/login',(req: Request, res: Response) => {
+    res.status(200).send("///// OK /////")
+})
 
 export default router
