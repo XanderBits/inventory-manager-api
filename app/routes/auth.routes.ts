a3285bb28bd315d85ba35c278c94b4479ee4696f
XanderBits <amarettosyntax@protonmail.com>
2022-05-28 19:13:05 -0400

    [ADD] Login controller to route

--- app/routes/auth.routes.ts
+++ app/routes/auth.routes.ts
@@ -1,11 +1,10 @@
 import { Request, Response, Router } from "express"
 import  {register} from '../controllers/auth/register'
+import  { login } from '../controllers/auth/login'
 import { validateRegister } from '../controllers/auth/validators/validateRegister'
 const router = Router()
 
 router.post('/auth/register', validateRegister, register)
-router.post('/auth/login',(req: Request, res: Response) => {
-    res.status(200).send("///// OK /////")
-})
+router.post('/auth/login', login)
 
 export default router
